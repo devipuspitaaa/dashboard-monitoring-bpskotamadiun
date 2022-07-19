8e6eb7bf57c59f95820b2d09990b9e09e605f26a
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-07-19 14:07:24 +0700

    perbaikan form login

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -2,10 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SIMENIK | Sistem Monitoring dan Evaluasi Kelurahan Cantik</title>
+
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
@@ -15,15 +12,12 @@
 </head>
 
 <body>
+    
     <div id="preloder">
         <div class="loader"></div>
     </div>
-    @include('layouts.footer')
+   
 </body>
 
-<!-- JS -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="js/main.js"></script>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,36 @@
-@extends('layouts.app2')
 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>SIMENIK | Sistem Monitoring dan Evaluasi Kelurahan Cantik</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        
+    </head>
+<body id="page-top">
+<!-- Navigation-->
 @include('layouts.nav')
-<div class="main">
+        <!-- Masthead-->
+
+<header class="masthead">
+
     <div class="container">
+   
         <div class="sign-up-content">
+       
             <form method="POST" class="signup-form">
+                
                 <h1 class="form-title">W E L C O M E</h1>
                 <div class="form-textbox">
                     <label for="username"> Username</label>
@@ -32,4 +58,18 @@
         </div>
     </div>
 
-</div>
+</header>
+ <!-- Footer-->
+ @include('layouts.footer')
+
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<!-- * *                               SB Forms JS                               * *-->
+<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+</html>
