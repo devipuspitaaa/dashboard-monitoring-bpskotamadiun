ec79a432ac1ad3a8613cbe769d48156eb3df78ac
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-07-20 10:00:46 +0700

    ubah file dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.landingPage');
 });
-Route::get('/login', function () {
-    return view('login');
-});
+// Route::get('/login', function () {
+//     return view('login');
+// });
 
 Route::get('/register', function () {
     return view('register');
@@ -27,3 +27,5 @@ Route::get('/register', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
