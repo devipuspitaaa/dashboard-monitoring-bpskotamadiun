0ba1e168784c3aebe37255ea0200294842f78163
SriKynanti <kynanti.ti@gmail.com>
2022-07-20 12:58:46 +0700

    Edit Footer & Navbar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,13 +3,13 @@
         <div class="row align-items-center">
             <div class="col-lg-4 text-lg-start">Copyright &copy; Badan Pusat Statistik Kota Madiun 2022</div>
             <div class="col-lg-4 my-3 my-lg-0">
-                <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/bpskotamadiun/" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/bpsstatistics/" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/bps_statistics" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/bpskotamadiun/" target="_blank" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/bpsstatistics/" target="_blank" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/bps_statistics" target="_blank" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="https://madiunkota.bps.go.id/" target="_blank" aria-label="Globe"><i class="fa fa-globe"></i></a>
             </div>
             <div class="col-lg-4 text-lg-end">
-                <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+                <a class="link-dark text-decoration-none me-3" href="https://madiunkota.bps.go.id/" target="_blank">Website BPS Kota Madiun</a>
             </div>
         </div>
     </div>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
                 <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#portfolio">Tentang</a></li>
+                <li class="nav-item"><a class="nav-link" href="/tentang">Tentang</a></li>
                 <li class="nav-item"><a class="nav-link" href="#about">Aplikasi</a></li>
             </ul>
         </div>
