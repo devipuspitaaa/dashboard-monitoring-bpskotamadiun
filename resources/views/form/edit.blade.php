45a0becb276e2c078a6de561c619f9a430dfec06
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-08-01 16:17:21 +0700

    edit data survei

--- resources/views/form/edit.blade.php
+++ resources/views/form/edit.blade.php
@@ -1,7 +1,3 @@
-@extends('template')
-@section('content')
-<br></br>
-<br></br>
 
     <section class="featured spad">
         <div class="container">
@@ -22,69 +18,81 @@
                             </div>
                         @endif
                     </div>
-                    <form method="post" action="{{ route('form.store') }}" id="myForm">
+                    <form method="post" action="{{ route('form.update', $data->id) }}" id="myForm">
                         @csrf
                         @method('PUT')
                         <div class="featured__controls">
-                            {{-- <div class="form-group">
-                                <label for="id">Id : </label>
-                                <input  type="text" name="id" class="form-control" id="disabledTextInput" aria-describedby="id" >
-                            </div> --}}
-
-                            <div class="form-group">
-                                <label for="nama_kelurahan">Nama Kelurahan</label>
-                                <input type="nama_kelurahan" name="nama_kelurahan" class="form-control" id="nama_kelurahan" aria-describedby="nama_kelurahan" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Nama Kelurahan</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="nama_kelurahan" name="nama_kelurahan" class="form-control" id="nama_kelurahan" value="{{$data->nama_kelurahan}}" aria-describedby="nama_kelurahan" placeholder="masukkan nama keluharan" required>
+                                    
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="nama_survei">Nama Survei</label>
-                                <input type="nama_survei" name="nama_survei" class="form-control" id="nama_survei" aria-describedby="nama_survei" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Nama Survei</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="nama_survei" name="nama_survei" class="form-control" id="nama_survei" value="{{$data->nama_survei}}" aria-describedby="nama_survei" placeholder="masukkan nama survei" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="total_target">Total Target</label>
-                                <input type="total_target" name="total_target" class="form-control" id="total_target" aria-describedby="total_target" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Total Target</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="total_target" name="total_target" class="form-control" id="total_target" value="{{$data->total_target}}" aria-describedby="total_target" placeholder="masukkan total target" required>
+                                </div>
                             </div>
+                        </div>
 
-
-                            <div class="form-group">
-                                <label for="ttl_target_revisi">Total Revisi</label>
-                                <input type="ttl_target_revisi" name="ttl_target_revisi" class="form-control" id="ttl_target_revisi" aria-describedby="ttl_target_revisi" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Total Petugas</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="total_petugas" name="total_petugas" class="form-control" id="total_petugas"value="{{$data->total_petugas}}"  aria-describedby="total_petugas" placeholder="masukkan total petugas" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="total_petugas">Total Petugas</label>
-                                <input type="total_petugas" name="total_petugas" class="form-control" id="total_petugas" aria-describedby="total_petugas" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Total Pengawas</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="total_pengawas" name="total_pengawas" class="form-control" id="total_pengawas" value="{{$data->total_pengawas}}" aria-describedby="total_pengawas" placeholder="masukkan total pengawas" required>
+                                </div>
                             </div>
-
-                            <div class="form-group">
-                                <label for="ttl_petugas_revisi">Total Petugas Revisi</label>
-                                <input type="ttl_petugas_revisi" name="ttl_petugas_revisi" class="form-control" id="ttl_petugas_revisi" aria-describedby="ttl_petugas_revisi" required>
+                        </div>
+                        
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Jangka Hari Penyelesaian</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="jh_penyelesaian" name="jh_penyelesaian" class="form-control" id="jh_penyelesaian" value="{{$data->jh_penyelesaian}}" aria-describedby="jh_penyelesaian" placeholder="masukkan jangka hari selesai" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="jh_penyelesaian">Jangka Hari Penyelesaian</label>
-                                <input type="jh_penyelesaian" name="jh_penyelesaian" class="form-control" id="jh_penyelesaian" aria-describedby="jh_penyelesaian" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Target Petugas</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="target_petugas" name="target_petugas" class="form-control" id="target_petugas" value="{{$data->target_petugas}}" aria-describedby="target_petugas" placeholder="masukkan target petugas" required>
+                                </div>
                             </div>
+                        </div>    
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    <a class="btn btn-success" href="{{ route('form.index') }}">Kembali</a>
 
-                            <div class="form-group">
-                                <label for="jhp_revisi">Jangka Hari Penyelesaian Revisi</label>
-                                <input type="jhp_revisi" name="jhp_revisi" class="form-control" id="jhp_revisi" aria-describedby="jhp_revisi" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="target_petugas">Target Petugas</label>
-                                <input type="target_petugas" name="target_petugas" class="form-control" id="target_petugas" aria-describedby="target_petugas" required>
-                            </div>
-
-                            <br><br>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                            <a class="btn btn-success" href="{{ route('form.index') }}">Kembali</a>
-
+                            </form>
                         </div>
                     </div>
                 </div>
 
 
-@endsection
+
 
