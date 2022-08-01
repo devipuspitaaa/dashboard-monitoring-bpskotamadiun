2aca31a0ce2d519eb14cae9ae3ecf3ae3e35e878
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-08-01 14:57:36 +0700

    tampilan input data survei

--- resources/views/form/inputForm.blade.php
+++ resources/views/form/inputForm.blade.php
@@ -28,51 +28,71 @@
                     <form method="post" action="{{ route('form.store') }}" id="myForm">
                         @csrf
                         <div class="featured__controls">
-                            {{-- <div class="form-group">
-                                <label for="id">Id : </label>
-                                <input  type="text" name="id" class="form-control" id="disabledTextInput" aria-describedby="id" >
-                            </div> --}}
-
-                            <div class="form-group">
-                                <label for="nama_kelurahan">Nama Kelurahan</label>
-                                <input type="nama_kelurahan" name="nama_kelurahan" class="form-control" id="nama_kelurahan" aria-describedby="nama_kelurahan" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Nama Kelurahan</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="nama_kelurahan" name="nama_kelurahan" class="form-control" id="nama_kelurahan" aria-describedby="nama_kelurahan" placeholder="masukkan nama keluharan" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="nama_survei">Nama Survei</label>
-                                <input type="nama_survei" name="nama_survei" class="form-control" id="nama_survei" aria-describedby="nama_survei" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Nama Survei</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="nama_survei" name="nama_survei" class="form-control" id="nama_survei" aria-describedby="nama_survei" placeholder="masukkan nama survei" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="total_target">Total Target</label>
-                                <input type="total_target" name="total_target" class="form-control" id="total_target" aria-describedby="total_target" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Total Target</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="total_target" name="total_target" class="form-control" id="total_target" aria-describedby="total_target" placeholder="masukkan total target" required>
+                                </div>
                             </div>
+                        </div>
 
-
-                            <div class="form-group">
-                                <label for="total_petugas">Total Petugas</label>
-                                <input type="total_petugas" name="total_petugas" class="form-control" id="total_petugas" aria-describedby="total_petugas" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Total Petugas</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="total_petugas" name="total_petugas" class="form-control" id="total_petugas" aria-describedby="total_petugas" placeholder="masukkan total petugas" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="total_pengawas">Total Pengawas</label>
-                                <input type="total_pengawas" name="total_pengawas" class="form-control" id="total_pengawas" aria-describedby="total_pengawas" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Total Pengawas</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="total_pengawas" name="total_pengawas" class="form-control" id="total_pengawas" aria-describedby="total_pengawas" placeholder="masukkan total pengawas" required>
+                                </div>
                             </div>
-
-                            <div class="form-group">
-                                <label for="jh_penyelesaian">Jangka Hari Penyelesaian</label>
-                                <input type="jh_penyelesaian" name="jh_penyelesaian" class="form-control" id="jh_penyelesaian" aria-describedby="jh_penyelesaian" required>
+                        </div>
+                        
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Jangka Hari Penyelesaian</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="jh_penyelesaian" name="jh_penyelesaian" class="form-control" id="jh_penyelesaian" aria-describedby="jh_penyelesaian" placeholder="masukkan jangka hari selesai" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="target_petugas">Target Petugas</label>
-                                <input type="target_petugas" name="target_petugas" class="form-control" id="target_petugas" aria-describedby="target_petugas" required>
+                        <div class="row">
+                            <label class="col-sm-2 col-form-label">Target Petugas</label>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <input type="target_petugas" name="target_petugas" class="form-control" id="target_petugas" aria-describedby="target_petugas" placeholder="masukkan target petugas" required>
+                                </div>
                             </div>
-
-                            <br><br>
+                        </div>    
                             <button type="submit" class="btn btn-primary">Submit</button>
 
-
+                            </form>
                         </div>
                     </div>
                 </div>
