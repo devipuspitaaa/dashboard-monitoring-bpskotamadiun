544059b6ffb078ddd6b8fabb8d278668756fd57d
fitriMD <mutiara.devi027@gmail.com>
2022-08-02 09:42:08 +0700

    role user

---
+++ database/migrations/2022_08_02_012942_tambah_kolom_role_di_tabel_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TambahKolomRoleDiTabelUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table){
+            $table->enum('role', ['admin', 'pengawas', 'lurah'])->after('password')->default('pengawas');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+}
