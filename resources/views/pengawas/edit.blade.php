1bb0d6b8074779da296aece69898faa201dd0550
SriKynanti <kynanti.ti@gmail.com>
2022-08-08 08:48:03 +0700

    edit pengawas page

--- resources/views/pengawas/edit.blade.php
+++ resources/views/pengawas/edit.blade.php
@@ -40,9 +40,25 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="jenis_kelamin">Jenis Kelamin</label>
-                        <input type="jenis_kelamin" name="jenis_kelamin" class="form-control" id="jenis_kelamin"
-                            value="{{$data->jenis_kelamin}}" aria-describedby="jenis_kelamin">
+                        <label for="tempat_tanggal_lahir">Jenis Kelamin</label>
+                        <div class="col-sm-10 checkbox-radios">
+                            <div class="form-check-radio">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="jenis_kelamin"
+                                        value="L" required>
+                                    Laki - Laki
+                                    <span class="form-check-sign"></span>
+                                </label>
+                            </div>
+                            <div class="form-check-radio">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="jenis_kelamin"
+                                        value="P" required>
+                                    Perempuan
+                                    <span class="form-check-sign"></span>
+                                </label>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="form-group">
