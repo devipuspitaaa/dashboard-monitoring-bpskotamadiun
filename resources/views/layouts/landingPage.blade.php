569566b97fc42a2d05913b30265799ac9b491863
fitriMD <mutiara.devi027@gmail.com>
2022-08-10 07:36:28 +0700

    edit landing page

--- resources/views/layouts/landingPage.blade.php
+++ resources/views/layouts/landingPage.blade.php
@@ -18,20 +18,55 @@
     <link href="css/styles.css" rel="stylesheet" />
 </head>
 
-<body id="page-top">
+<body>
     <!-- Navigation-->
-    @include('layouts.nav')
-    <!-- Masthead-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand">Portal OPERA CANDI</a>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link scrollto active" href="{{ url('/') }}">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#tentang">Tentang</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Aplikasi</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    {{-- @include('layouts.nav') --}}
+  <!-- ======= Home Section ======= -->  
+    
     <header class="masthead">
         <div class="container">
             <div class="masthead-heading">SELAMAT DATANG di Portal Monitoring & Evaluasi</div>
             <div class="masthead-heading">OPERA CANDI</div>
             <div class="masthead-subheading">(Optimalisasi Petugas Kelurahan Cinta Statistik Kota Madiun)</div>
-
-
-            <a class="btn btn-primary btn-xl text-uppercase" href="/login">Mulai</a>
+            <a class="btn btn-primary btn-xl text-uppercase" href="{{ route('login') }}">Mulai</a>
         </div>
-    </header>
+    </header> <!-- End Home -->
+
+    <!-- ======= About Section ======= -->
+    <section id="tentang" class="tentang">
+        <div class="container">
+            <i class="bi bi-geo-fill"></i>
+            <center><h3>PETA KOTA MADIUN</h3></center>
+            <br>
+            <div class="embed-responsive embed-responsive-21by9">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63271.893040272866!2d111.49303169146093!3d-7.629975418913439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e79be537c813a33%3A0xafe2f173545a53ae!2sMadiun%2C%20Kota%20Madiun%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1658296052969!5m2!1sid!2sid"
+                    width="1100" height="800" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <br><br>
+                <div>
+                    <center><h5>Madiun (bahasa Jawa: Hanacaraka: ꦩꦝꦶꦪꦸꦤ꧀, Pegon: مادييون, translit. Madhiyun)
+                        adalah sebuah kota di Provinsi Jawa Timur, Indonesia. Kota ini terletak 160 km sebelah barat
+                        Surabaya, atau 111 km sebelah timur Surakarta, Jawa Tengah. Di kota ini terdapat Industri kereta
+                        api (INKA) dan memiliki sekolah tinggi perkeretaapian, yakni salah satunya Politeknik Perkeretaapian
+                        Indonesia. Kota Madiun mendapat julukan sebagai "Kota Gadis", "Kota Brem", "Kota Pecel", "Kota Budaya",
+                        "Kota Industri", "Kota Karismatik", dan "Kota Pendekar".</h5></center>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <!-- Footer-->
     @include('layouts.footer')
