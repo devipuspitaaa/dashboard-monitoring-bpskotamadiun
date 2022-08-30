ef20a2255a05c7981996f199078b32ad19efe140
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-08-30 14:58:09 +0700

    dashboard grafik

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -78,8 +78,73 @@ class HomeController extends Controller
 
         // area ---- komulatif
         $dt_survey = DB::table("survei")->first();
+        $dt_statistik = $this->statistik();
+        
+        return view('home', compact('dt_entry', 'kolomTable', 'dt_survey', 'dt_statistik'));
+    }
 
 
-        return view('home', compact('dt_entry', 'kolomTable', 'dt_survey'));
+
+    function statistik() {
+
+        // ambil data total target
+        $chart = [];
+
+
+        $dt_target = DB::table("survei");
+        
+        // cek apakah sudh terisi
+        if ( $dt_target->count() > 0 ) {
+
+            $isi = $dt_target->first();
+
+            $totalPengawas = $isi->total_pengawas;
+            $totalPetugas  = $isi->target_petugas;
+
+            $target = $totalPengawas * $totalPetugas;
+
+            // data realisasi target
+            $dt_realisasi = DB::table("targets")->groupBy("tanggal")->get();
+            
+            // nilai temporary 
+            $temp_target = 0;
+            $temp_realisasi = 0;
+
+
+            foreach ( $dt_realisasi AS $urutan => $kolom ) {
+
+                // ambil data realisasi target detail berdasarkan tanggal
+                $dt_realisasi_by_tanggal = DB::table("targets")->where(["tanggal" => $kolom->tanggal])->get();
+                $total_realisasi = 0;
+                foreach ( $dt_realisasi_by_tanggal AS $kolom_realisasi ) {
+
+                    $total_realisasi += $kolom_realisasi->target;
+                }
+
+
+                // append
+                if ( $urutan == 0 ) {
+
+                    $temp_target = $target;
+                    $temp_realisasi = $total_realisasi;
+                } else {
+
+                    $temp_target += $target;
+                    $temp_realisasi += $total_realisasi;
+                }
+
+                
+                array_push($chart, array(
+
+                    'tanggal'   => $kolom->tanggal,
+                    'target'    => $temp_target,
+                    'realisasi' => $temp_realisasi
+                ));
+            }
+        }
+
+
+        
+        return $chart;
     }
 }
